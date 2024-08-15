943a8bdcddbc0bc7dd481f7960584595650864e9
phillipdinh <phillipdinh11@yahoo.com>
2024-08-15 14:34:26 -0700

    Added Game Over validation

--- src/Board.js
+++ src/Board.js
@@ -1,20 +1,73 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import Cell from "./Cell"
 
 import "./styles.css"
 export default function Board() {
 	const [board, setBoard] = useState(boardInit())
+	const [isGameOver, setIsGameOver] = useState(false)
 
-	const handleBoardClick = (row, col) => {
-		const clickedCell = board[row][col]
+	const isPlayerXTurn = useRef(true)
+
+	const handleBoardClick = (index) => {
+		const clickedCell = board[index]
+
+		if (clickedCell.player) {
+			console.log("Already Picked")
+			return
+		}
+
+		const newBoard = board.map((cell) => ({ ...cell }))
+
+		if (isPlayerXTurn.current) {
+			newBoard[index].player = "X"
+		} else {
+			newBoard[index].player = "O"
+		}
+
+		setBoard(newBoard)
+
+		isPlayerXTurn.current = !isPlayerXTurn.current
+
+		//TODO add gameOver component
+		hasAPlayerWon(newBoard, newBoard[index].player)
+	}
+
+	const winIndexes = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	]
+	function hasAPlayerWon(board, player) {
+		for (let winRow of winIndexes) {
+			for (let i = 0; i < 3; i++) {
+				if (board[winRow[i]].player !== player) break
+
+				if (i === 2) {
+					console.log("Game Over", player, "Won")
+					setIsGameOver((prev) => !prev)
+				}
+			}
+		}
 	}
 	return (
 		<div className="board-container">
 			<div className="board">
 				{board.map((cell, index) => (
-					<Cell key={index} index={cell.index} player={cell.player} />
+					<Cell
+						key={index}
+						index={cell.index}
+						player={cell.player}
+						handleClick={() => handleBoardClick(cell.index)}
+					/>
 				))}
 			</div>
+
+			{isGameOver ? <div>Game Over</div> : <></>}
 		</div>
 	)
 }
--- src/Cell.js
+++ src/Cell.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react"
 
 export default function Cell(props) {
-	return <div className="cell" id={`cell-${props.index}`}></div>
+	return (
+		<div className="cell" id={`cell-${props.index}`} onClick={props.handleClick}>
+			{props.player}
+		</div>
+	)
 }
